f52cc89603903e36835bc84f6459881cbe404134
Ttands-168 <Tonydstan@gmail.com>
2025-01-10 11:00:26 +0000

    Lesson 1's End

--- CT06_01/lesson1.py
+++ CT06_01/lesson1.py
@@ -2,7 +2,7 @@ print("Hello from lesson 1")
 print(2025)
 print("I am learning to code in Computhink.")
 
-######### The below is the code required for task 2a ##########
+######### The below is the code required for task 1 ##########
 # print("1")
 # print("2")
 # print("3")
@@ -58,6 +58,32 @@ print("10 January 2025")
 # print("2029")
 # print("2030")
 
-############## Below is the code for task 2b ##############
+############## Below is the code for task 2 ##############
 
+print("Hello World!")
 print("I am born on June 26, 2010.")
+print("I love xiao long bao.")
+print("I study in Pathlight.")
+print("If I had $800 to spend in JB, I can spend on hotel stays near South Key.")
+
+# Print task 3
+
+print("Wake up!")
+print("Eat breakfast")
+print("Brush teeth")
+print("Dress up")
+print("Leave house by 6:45 AM")
+
+# Correct the errors
+
+print("Wake up and get out of bed.")
+print("Eat your breakfast.")
+print("Pour cornflakes and choco milk into a bowl.")
+
+print("Hello and how are you?")
+
+print("What is wrong with this")
+
+print("Why is this not working?")
+
+print("Why is this not working?")
