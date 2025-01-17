43bedb1d1dd7d7e2cf875dfb908d2fc45d16cad4
Ttands-168 <Tonydstan@gmail.com>
2025-01-17 10:18:57 +0000

    Jan 17, 2025, 10:18 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -19,6 +19,8 @@ print(colours_of_the_rainbow)
 # else:
 #     say("You are eligible.")
 
+
+
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
 
