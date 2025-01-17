50a13760463871a6466a30079ea8a69118bf05eb
Ttands-168 <Tonydstan@gmail.com>
2025-01-17 10:14:05 +0000

    Jan 17, 2025, 10:14 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -13,10 +13,11 @@ print(colours_of_the_rainbow)
 #     turn 15
 #     counter = counter += 10
 
-say("How old are you?")
-if age < 18:
-    say("You are not eligible.")
-
+# say("How old are you?")
+# if age < 18:
+#     say("You are not eligible.")
+# else:
+#     say("You are eligible.")
 
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
