8a03550fcf1f892b94af3c4853af6406d286773b
Ttands-168 <Tonydstan@gmail.com>
2025-01-17 10:40:48 +0000

    Jan 17, 2025, 10:40 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -38,4 +38,4 @@ print(colours_of_the_rainbow)
 test1 = float(input("What is your WA1 score?"))
 test2 = float(input("What is your WA2 score?"))
 test3 = float(input("What is your WA3 score?"))
-final_score = 0.2*test1 + 0.4*test2 
+final_score = 0.2*test1 + 0.4*test2 + 0.4*test3
