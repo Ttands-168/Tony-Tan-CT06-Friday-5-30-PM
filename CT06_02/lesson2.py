22fe9cd03fbedb1d967fe3299416812406b7aca8
Ttands-168 <Tonydstan@gmail.com>
2025-01-17 09:58:29 +0000

    Jan 17, 2025, 9:58 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -3,6 +3,8 @@ print("Hello from lesson 2")
 colours_of_the_rainbow = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"]
 print(colours_of_the_rainbow)
 
+
+
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
 
