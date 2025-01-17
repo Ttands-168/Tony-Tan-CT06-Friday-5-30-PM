ec152ee5c6242883746358985a655bba52fd783c
Ttands-168 <Tonydstan@gmail.com>
2025-01-17 10:11:14 +0000

    Jan 17, 2025, 10:11 AM

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -3,7 +3,17 @@ print("Hello from lesson 2")
 colours_of_the_rainbow = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"]
 print(colours_of_the_rainbow)
 
-# repeat 10 tim
+# 10.times:
+#     say "Hey"
+#     step 10
+
+# counter = 0
+# while counter is not 50
+#     step 10
+#     turn 15
+#     counter = counter += 10
+
+
 
 ######## Write the pseudocode in comments for task 2 here
 # Using comments, translate the code shown on screen into pseudocode.
