b11fa238b33acc19fbc42d9240a9cd7a83b45c42
Ttands-168 <Tonydstan@gmail.com>
2025-02-07 09:46:20 +0000

    Feb 7, 2025, 9:46 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -9,4 +9,4 @@ customer2 = yellow_plate * 2 + cyan_plate * 5
 customer3 = orange_plate * 20 + cyan_plate
 print("Noor Daanya pays $" + str(customer1) + " for this.")
 print("Khoo Siew Ling pays $" + str(customer2) + " for this.")
-print("")
+print("Smahon Loh is ")
