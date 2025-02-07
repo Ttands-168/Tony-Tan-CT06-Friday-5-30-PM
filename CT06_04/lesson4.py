83f2716e7b54c81a96cd9bc6b7e59a32478bdc1a
Ttands-168 <Tonydstan@gmail.com>
2025-02-07 09:43:22 +0000

    Feb 7, 2025, 9:43 AM

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -5,5 +5,5 @@ cyan_plate = 4.5
 real_turquoise_plate = 6
 
 customer1 = orange_plate * 3 + yellow_plate * 5 + green_plate * 4
-customer2 = 
+customer2 = yellow_plate * 2 + 
 print("Noor Daanya pays $" + str(customer1) + "for this.")
