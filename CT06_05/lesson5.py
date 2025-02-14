af38352e31b80550bfaef7ac662b24731686e221
Ttands-168 <Tonydstan@gmail.com>
2025-02-14 10:11:36 +0000

    Feb 14, 2025, 10:11 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -10,6 +10,3 @@ for counterstrike in range(1,6):
     print("Turanika patros kamistan.")
 print("Outside 7.")
 
-# Consequence for talking to Jared and Aidan
-for consequence in range(1,501):
-    print("I will ignore Jared and Aidan and give them some space.")
