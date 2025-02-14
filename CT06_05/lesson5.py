0fd576e079f52878e1a7ad3b07e6a532510e58df
Ttands-168 <Tonydstan@gmail.com>
2025-02-14 10:18:15 +0000

    Feb 14, 2025, 10:18 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -16,3 +16,4 @@ for Rose in Trump:
     print("Don't you want me like I want you baby, don't you leave me like I leave you now, sleep tomorrow but tonight go crazy, all you gotta do is just meet me at the " + Rose)
 print("Make America great again!")
 
+# Task 1
