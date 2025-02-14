06bf6b0b5066f4509e0dc2d886fa5d1ae5f63823
Ttands-168 <Tonydstan@gmail.com>
2025-02-14 10:01:41 +0000

    Feb 14, 2025, 10:01 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -3,4 +3,4 @@ yourName = input("What is your name?")
 yourAge = int(input("How old are you turning this year?"))
 theMessage = ("Happy money!")
 
-print(yourName + str(yourAge) + theMessage)
+print("Hey " + yourName + ", you are turning " + str(yourAge) + " and " + theMessage + ".")
