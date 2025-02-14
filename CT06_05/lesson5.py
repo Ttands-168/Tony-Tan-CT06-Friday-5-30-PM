9343ed4f1d6310a6994e2ea5dc4b3d68038e2ef4
Ttands-168 <Tonydstan@gmail.com>
2025-02-14 10:20:24 +0000

    Feb 14, 2025, 10:20 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -20,4 +20,4 @@ print("Make America great again!")
 Jared = "Jared"
 for encouragement in Jared:
     print("Go for the " + Jared + "!")
-print("But Eugene ")
+print("Come on Jared! You can do better than this!")
