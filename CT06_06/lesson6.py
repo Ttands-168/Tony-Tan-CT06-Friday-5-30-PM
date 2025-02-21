b9b572e3b5f7995adeccbd2bccb5e2e27c793d85
Ttands-168 <Tonydstan@gmail.com>
2025-02-21 09:44:35 +0000

    Feb 21, 2025, 9:44 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -3,3 +3,8 @@ num1 = input("Give me your start value. ")
 num2 = input("Give me your end value. ")
 
 num1 = float(num1)
+num2 = float(num2)
+
+if num1 > num2:
+    for Russia in range(num2, num1):
+        print()
