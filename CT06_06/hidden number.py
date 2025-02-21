54f297f3089e4b3b5791ac2104cdc58db00a79d4
Ttands-168 <Tonydstan@gmail.com>
2025-02-21 11:02:36 +0000

    Feb 21, 2025, 11:02 AM

--- CT06_06/hidden number.py
+++ CT06_06/hidden number.py
@@ -9,4 +9,16 @@ tries = 5
 while tries > 0:
     user_input = float(input("What is your number?"))
     if user_input > 38 or user_input < 0:
-        
+        print("This is not a valid input.")
+    else:
+        if user_input > random_float + 0.25:
+            print("This is too large - guess a smaller number.")
+            tries = tries - 1
+        elif user_input < random_float - 0.25:
+            print("This is too small - guess a smaller number.")
+            tries = tries - 1
+        else:
+            print("Good job! You got the right answer! The float is " + str(random_float) + ".")
+            break
+print("Game Over!")
+print("The number is " + str(random_float) + ".")
