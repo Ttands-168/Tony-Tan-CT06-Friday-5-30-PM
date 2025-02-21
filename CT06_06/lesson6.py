c6186598236d0a096ca3e3ba863ebcbf050f6083
Ttands-168 <Tonydstan@gmail.com>
2025-02-21 10:07:26 +0000

    Feb 21, 2025, 10:07 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -18,5 +18,5 @@ Geography_Sec3 = int(input("How many students are in your class? "))
 total_test_grade = 0
 for students in range(1, Geography_Sec3 + 1):
     username = input("What is your name? ")
-    test_grade = float(input("What is your score for geography? "))
-    total_test_grade = total_test_grade + test_grade
+    individual_test_grade = float(input("What is your score for geography? "))
+    total_test_grade = total_test_grade + individual_test_grade
