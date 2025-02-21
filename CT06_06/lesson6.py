3dbba17c6de75ccc22c529bf840c4db1c2b1ccea
Ttands-168 <Tonydstan@gmail.com>
2025-02-21 10:05:20 +0000

    Feb 21, 2025, 10:05 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -18,3 +18,4 @@ Geography_Sec3 = int(input("How many students are in your class? "))
 for students in range(1, Geography_Sec3 + 1):
     username = input("What is your name? ")
     test_grade = float(input("What is your score for geography? "))
+total_count = ()
