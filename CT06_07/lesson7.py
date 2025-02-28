32bf8dd73732c5a3010c54e180b63bd78aa96487
Ttands-168 <Tonydstan@gmail.com>
2025-02-28 09:54:16 +0000

    Feb 28, 2025, 9:54 AM

--- CT06_07/lesson7.py
+++
@@ -1,8 +0,0 @@
-score_one = 80
-score_two = 90
-score_three = 75
-total = score_one + score_two + score_three
-average_score = total / 3
-student_name = "Alex"
-print("Average score for " + student_name + " is: " + str(average_score))
-
