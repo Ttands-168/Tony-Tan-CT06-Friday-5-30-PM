41f0c205595870b29f46b20d526c8ffb481e2f5b
Ttands-168 <Tonydstan@gmail.com>
2025-03-07 10:39:00 +0000

    Mar 7, 2025, 10:39 AM

--- CT06_08/Lesson 8.py
+++ CT06_08/Lesson 8.py
@@ -1,5 +1,5 @@
 product = 1
 for i in range(5):
-    multiply = float(input("What number are you looking at?"))
+    multiply = float(input("What number are you looking at? "))
     product = product * multiply
 print(product)
