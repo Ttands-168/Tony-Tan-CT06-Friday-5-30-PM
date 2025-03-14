4a451e8a64bc0db112995dc1b74736ddf742baa6
Ttands-168 <Tonydstan@gmail.com>
2025-03-14 09:57:51 +0000

    Mar 14, 2025, 9:57 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -1,7 +1,7 @@
 import time
 import math
 import random
-Sleep_Time = int(input("How many seconds do you want me to count down?"))
+Sleep_Time = int(input("How many seconds do you want me to count down? "))
 for i in range(Sleep_Time, 0, -1):
     print(i)
 print("Happy New Year!")
