2774224237ffb9e29e38620162550c46ee65859c
Ttands-168 <Tonydstan@gmail.com>
2025-03-14 11:00:23 +0000

    Mar 14, 2025, 11:00 AM

--- CT06_09/lesson9.py
+++ CT06_09/lesson9.py
@@ -23,7 +23,7 @@ import random
 
 user_input1 = float(input("Give me the start number. "))
 user_input2 = float(input("Give me the end number. "))
-the_print = random.randint(1,37)
+the_print = random.random() * 3
 if user_input1 <= the_print <= user_input2:
     print(True)
 else:
