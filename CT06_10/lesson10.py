d3235d84f48756839341c2949016e51852108b1f
Ttands-168 <Tonydstan@gmail.com>
2025-03-28 10:45:31 +0000

    Mar 28, 2025, 10:45 AM

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -20,4 +20,5 @@ temperature = float(input("What is the temperature outside today? "))
 if temperature < 16:
     print("You can play computer games or read or do your homework or something.")
 elif temperature < 31:
-    print("It's time")
+    print("It's time to go outside and play.")
+    
