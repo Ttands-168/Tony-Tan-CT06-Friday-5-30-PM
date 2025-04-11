e6d0815332d457e0eacdee50e3cee58b15e58383
Ttands-168 <Tonydstan@gmail.com>
2025-04-11 09:53:37 +0000

    Apr 11, 2025, 9:53 AM

--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -46,5 +46,5 @@ if 天 != "Friday" or 天 != "Saturday" or 天 != "Sunday":
 else:
     print("Yay!")
 
-main_meal = input("What do you want to eat your main as? ")
+main_meal = input("What do you want to order? ")
 sides = input("")
