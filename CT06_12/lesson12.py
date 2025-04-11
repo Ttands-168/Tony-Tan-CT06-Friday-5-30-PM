2b2c62383308dcf35a869397f01aebbf1d5b5556
Ttands-168 <Tonydstan@gmail.com>
2025-04-11 10:32:54 +0000

    Apr 11, 2025, 10:32 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -3,8 +3,7 @@ found_e = False
 found_o = False
 while not(found_e and found_o and len(word) == 5):
     print("Got it! " + word + ".")
-else:
-    print("Not a good choice.")
+    
 
 counter = 60
 while counter > 0:
