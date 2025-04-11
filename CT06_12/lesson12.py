11ab21d9d05c35b1ceb9991f03577f2da600ccbc
Ttands-168 <Tonydstan@gmail.com>
2025-04-11 10:33:44 +0000

    Apr 11, 2025, 10:33 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -1,9 +1,9 @@
-
 found_e = False
 found_o = False
 while True:
     word = input("Gimme a word. ")
     while not(found_e and found_o and len(word) == 5):
+        
         print("Got it! " + word + ".")
 
 
