6c41ff3ee7c3e6cd89e1d963feccd89a26c84843
Ttands-168 <Tonydstan@gmail.com>
2025-04-14 01:04:55 +0000

    Apr 14, 2025, 1:04 AM

--- CT06_12/14 April Computing.py
+++ CT06_12/14 April Computing.py
@@ -3,4 +3,5 @@ inflation = 0.024
 print("Inflation in US is " + str(inflation * 100) + "% in March 2025.")
 # This is a comment
 # Think about your comments before you say it out.
-# Whether is it 
+# Whether is it True
+# 
