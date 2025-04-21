ee8d2c8d9835f6a2a1e72c2e82428122c6f43049
Ttands-168 <Tonydstan@gmail.com>
2025-04-21 08:00:48 +0000

    Apr 21, 2025, 8:00 AM

---
+++ CT06_12/21 April Computing.py
@@ -0,0 +1,51 @@
+x = 456
+x += 155
+print(x)
+x -= 488
+x -= 87
+print(x)
+x = 256
+x /= 8
+print(x)
+x += 15
+x *= 13
+print(x)
+x -= 548
+x /= 21
+x += 6
+print(x)
+
+del x
+
+n = 47//5
+print(n)
+n += 15
+n %= 5
+print(n)
+n /= 2
+n **= 4
+print(n)
+
+del n
+
+a = 15
+b = 7
+c = 5
+d = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(c // d)
+print(c % d)
+print(c ** d)
+
+del a
+del b
+del c
+del d
+
+a = 5
+b = 6.0
+print(a + b)
