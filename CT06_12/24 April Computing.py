9bbe153393c805e41686f24e1b7215e25e9e50be
Ttands-168 <Tonydstan@gmail.com>
2025-04-24 01:21:34 +0000

    Apr 24, 2025, 1:21 AM

---
+++ CT06_12/24 April Computing.py
@@ -0,0 +1,44 @@
+print('Hello World'.endswith('ld'))
+print('2025'.startswith('20'))
+print('CS2025'.find('20'))
+print('CS2025'.find('21'))
+print('A AA AAA'.split())
+print('A AA AAA'.split(' '))
+print('Index,5, 88,3!'.split(','))
+print('PIZza'.lower())
+a = 'Hello'
+print(a.upper())
+print('PIZza'.islower())
+print('CS2025'.isupper())
+print('   '.isspace())
+print('  Computing'.isspace())
+print('Computing2025'.isalnum())
+print('Computing 2025'.isalnum())
+print('computing'.isalpha())
+print('CS2025'.isalpha())
+print('2025'.isdigit())
+print('2025   '.isdigit())
+
+x = 'Hello'
+y = "World"
+print('Hello' + "World")
+print(x + y)
+print(x * 2)
+print(2 * x)
+print(x + " " + y)
+
+p = "ba"
+q = "na"
+print(p + q * 2)
+print("man" in "maneater")
+print("eat" not in "theatre")
+print(len("maneater"))
+
+s = "Computing"
+s[0]
+s[3]
+s[-2]
+s[0:2]
+s[1:2]
+s[:2]
+s[::2]
