e4e18e21b3c7bc2aa467e58418c257f818235b24
Ttands-168 <Tonydstan@gmail.com>
2025-04-25 10:48:11 +0000

    Apr 25, 2025, 10:48 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -9,7 +9,7 @@ while True:
         withdraw = float(input("How much money do you want to withdraw? "))
         user_balance -= withdraw
         if user_balance < 0:
-            print("You owe the bank $" + str(-user_balance) + ". If you do not pay by ")
+            print("You owe the bank $" + str(-user_balance) + ". If you do not pay by 7 days, you will get charged 2\")
     if options == "loan":
         loan = float(input("How much loan do you want to take out? "))
         user_balance -= loan
