d00b5317b40857c0566f1ef64d5d65897249be97
Ttands-168 <Tonydstan@gmail.com>
2025-04-25 10:44:09 +0000

    Apr 25, 2025, 10:44 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -1,7 +1,6 @@
 # Lesson 12 recap
 user_balance = 10
 while True:
-    
     options = input("Enter the options. ")
     if options == "deposit":
         deposit = float(input("How much money do you want to deposit? "))
