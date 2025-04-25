b918743956ca51892628b2921e9dfa2eb890766e
Ttands-168 <Tonydstan@gmail.com>
2025-04-25 10:23:59 +0000

    Apr 25, 2025, 10:23 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -53,7 +53,7 @@ while True:
         hidden_answer = number1 / number2
 
     if hidden_answer - 0.25 <= answer <= hidden_answer + 0.25:
-        
+        print("Correct!")
         score += 2
     else:
         print("Wrong! Try again!")
