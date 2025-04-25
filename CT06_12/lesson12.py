2c6c0f60a4dddc00f5df72865c61a0a005561223
Ttands-168 <Tonydstan@gmail.com>
2025-04-25 09:50:12 +0000

    Apr 25, 2025, 9:50 AM

--- CT06_12/lesson12.py
+++ CT06_12/lesson12.py
@@ -12,11 +12,12 @@ while counter > 0:
     print(counter)
     counter -= 1
 
-visitors_already_present = int(input("How many visitors are there? "))
-while visitors_already_present < 50:
-    print("You can add more.")
-else:
-    print("Sorry, we are full!")
+while True:
+    visitors_already_present = int(input("How many visitors are there? "))
+    while visitors_already_present < 50:
+        print("You can add more.")
+    else:
+        print("Sorry, we are full!")
 
 del visitors_already_present
 order = ""
