768479a4b70b748398b4c95c012c7beb8bdd5282
Ttands-168 <Tonydstan@gmail.com>
2025-04-25 10:35:33 +0000

    Apr 25, 2025, 10:35 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -5,4 +5,6 @@ while True:
     if options == "deposit":
         deposit = float(input("How much money do you want to deposit?"))
         user_balance += deposit
-    if options == "withdraw"
+    if options == "withdraw":
+        withdraw = float(input("How much money do you want to withdraw?"))
+        
