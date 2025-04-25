a12b11f9464fc47ff222c67df0f279f6c6617ab9
Ttands-168 <Tonydstan@gmail.com>
2025-04-25 10:39:23 +0000

    Apr 25, 2025, 10:39 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -3,13 +3,13 @@ while True:
     user_balance = 10
     options = input("Enter the options. ")
     if options == "deposit":
-        deposit = float(input("How much money do you want to deposit?"))
+        deposit = float(input("How much money do you want to deposit? "))
         user_balance += deposit
     if options == "withdraw":
-        withdraw = float(input("How much money do you want to withdraw?"))
+        withdraw = float(input("How much money do you want to withdraw? "))
         user_balance -= withdraw
     if options == "loan":
-        loan = float(input("How much loan do you want to take out?"))
+        loan = float(input("How much loan do you want to take out? "))
         user_balance -= loan
     if options == "check balance":
         print("You currently have $" + str(user_balance) + " in your account.")
