d74892361a65bb66ebf39e17db33e0ec62e3e91e
Ttands-168 <Tonydstan@gmail.com>
2025-05-01 08:49:21 +0000

    May 1, 2025, 8:49 AM

--- CT06_13/28 April-1 May Computing.py
+++ CT06_13/28 April-1 May Computing.py
@@ -37,7 +37,9 @@ print(result)
 
 del l[0]
 del l[1]
-del result, l[2], l[3], l[::2], l[::-1], l[::2][1], l[:999], l[:-999], l[:]
+del result
+del l[2]
+del l[3], l[::2], l[::-1], l[::2][1], l[:999], l[:-999], l[:]
 
 names = ['Beatrice', 'Celeste', 'Diana', 'Siewling', 'Jasmine']
 names[0] = 'Alex'
