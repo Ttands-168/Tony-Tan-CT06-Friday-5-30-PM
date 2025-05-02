7d03fd3c33f58e2107ac9759e7b83c4b53871a0e
Ttands-168 <Tonydstan@gmail.com>
2025-05-02 10:54:44 +0000

    May 2, 2025, 10:54 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -63,4 +63,4 @@ while number <= 168:
 else:
     print("End")
 
-catalogue = ["Celeste", "Diana", "Ethan Tian", "Kamala", "Terence", "Othman Tian", ""]
+catalogue = ["Celeste", "Diana", "Ethan Tian", "Kamala", "Terence", "Othman Tian", "Kim Jisoo"]
