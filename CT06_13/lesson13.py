9bd6b443d5061928493dca3abf87e1e822d43ddd
Ttands-168 <Tonydstan@gmail.com>
2025-05-02 10:20:26 +0000

    May 2, 2025, 10:20 AM

--- CT06_13/lesson13.py
+++ CT06_13/lesson13.py
@@ -24,7 +24,7 @@ while True:
 
 # Start of Lesson 13
 students = ["Aidan", "Alex", "Belinda", "Bernice", "Celeste Tan", "Celeste Kim", "Diana", "Ethan Tian", "Ethan Wong", "Felicia", "Glenda", "Kayson", "Koko", "Lisa", "Lucas", "Nancy", "Paulina", "Quincy", "Ravi", "Ryan", "Tony", "Vincent"]
-groceries = ["Banana", "Bacon", "Beef", "Chicken", "Cornflakes", "Dark Chocolate", "Eggs", "Falfel", "Honey", "Kale", "Whisky"]
+groceries = ["Banana", "Bacon", "Beef", "Chicken", "Cornflakes", "Dark Chocolate", "Eggs", "Falfel", "Honey", "Kale"]
 print(groceries)
 groceries.insert(3, "Bread")
 print(groceries)
