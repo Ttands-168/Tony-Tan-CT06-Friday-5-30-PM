75a4920a4c7d7da2e18a76339fc44379b34da16f
Ttands-168 <Tonydstan@gmail.com>
2025-05-16 10:41:30 +0000

    May 16, 2025, 10:41 AM

--- CT06_15/Lesson 15 general.py
+++ CT06_15/Lesson 15 general.py
@@ -5,4 +5,5 @@ Gareth_Gan.forward(80)
 kokowa_kochi_pia_sumida = turtle.Turtle()
 kokowa_kochi_pia_sumida.pendown()
 kokowa_kochi_pia_sumida.setpos(7, -59)
-kokowa_kochi_pia_sumida.forward
+for Celeste in range(3):
+    kokowa_kochi_pia_sumida
