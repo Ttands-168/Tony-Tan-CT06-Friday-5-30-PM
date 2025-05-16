27c3e3431c550c5632d6b8f7d74bda61e7036ba5
Ttands-168 <Tonydstan@gmail.com>
2025-05-16 10:37:03 +0000

    May 16, 2025, 10:37 AM

--- CT06_15/Lesson 15 general.py
+++ CT06_15/Lesson 15 general.py
@@ -1,5 +1,7 @@
 import turtle
 Gareth_Gan = turtle.Turtle()
+Gareth_Gan.set
 Gareth_Gan.forward(80)
 kokowa_kochi_pia_sumida = turtle.Turtle()
 kokowa_kochi_pia_sumida.pendown()
+kokowa_kochi_pia_sumida
