00ab5b193018de36650651c8d45ed6041d2c47e9
Ttands-168 <Tonydstan@gmail.com>
2025-05-16 10:04:23 +0000

    May 16, 2025, 10:04 AM

--- CT06_15/Python Turtle Lesson 14-15.py
+++ CT06_15/Python Turtle Lesson 14-15.py
@@ -4,7 +4,7 @@ Fiona.shape("rabbit")
 Fiona.speed(10)
 for x in range(1, 3):
     Fiona.forward(85)
-    turtle.right(90)
-    turtle.forward(20)
+    Fiona.right(90)
+    Fiona.forward(20)
     turtle.left(90)
     turtle.forward
