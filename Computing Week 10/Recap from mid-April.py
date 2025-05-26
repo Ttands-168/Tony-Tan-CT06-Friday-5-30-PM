31e66e664639012b083c04f616069d4b08e395c2
Ttands-168 <Tonydstan@gmail.com>
2025-05-26 00:31:19 +0000

    May 26, 2025, 12:31 AM

--- Computing Week 10/Recap from mid-April.py
+++ Computing Week 10/Recap from mid-April.py
@@ -1,7 +1,11 @@
-
+import time
 # Celeste and celeste are case-sensitive
 # Use underscores instead of hyphens in variable names
 box_size = 80 # not size_correct?
 print("Box size is", box_size)
 # You are discouraged from using break and continue in loops
 count = 10
+while count > 0:
+    print("Count is", count)
+    count -= 1
+    time.sleep(1)  # Sleep for 1 second to simulate a delay
