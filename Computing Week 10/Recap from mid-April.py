b63568000b11a9a031d87d6e58fe54ce0dae1e19
Ttands-168 <Tonydstan@gmail.com>
2025-05-26 00:42:45 +0000

    May 26, 2025, 12:42 AM

--- Computing Week 10/Recap from mid-April.py
+++ Computing Week 10/Recap from mid-April.py
@@ -24,4 +24,4 @@ India = 1,440,000,000  # Population of India
 China = 1,410,000,000  # Population of China
 United_States = 340,000,000  # Population of the United States 2025
 theta = x
-math.sin**2()
+math.sin**2(theta) + math.cos**2(theta) == 1  # Pythagorean identity
