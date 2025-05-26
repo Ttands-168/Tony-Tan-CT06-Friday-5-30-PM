82beb2e5f0303846a7e8ee690a2b4446a6e8b30d
Ttands-168 <Tonydstan@gmail.com>
2025-05-26 00:50:43 +0000

    May 26, 2025, 12:50 AM

--- Computing Week 10/Recap from mid-April.py
+++ Computing Week 10/Recap from mid-April.py
@@ -27,5 +27,5 @@ theta = x  # Use descriptive names for angles
 math.sin**2(theta) + math.cos**2(theta) == 1  # Pythagorean identity
 
 class_1940 = False # Because Germany was depressed in 1940
-Germany = 79_000_000  # Population of Germany in 1940
+Germany = 79,000,000  # Population of Germany in 1940
 Celeste = 56,200 # Population of Celeste in 2024 United States
