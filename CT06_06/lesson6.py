593470e23fa733bb016a090d038bf1ccf525bcee
Ttands-168 <Tonydstan@gmail.com>
2025-05-26 00:38:35 +0000

    May 26, 2025, 12:38 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -23,53 +23,53 @@ for students in range(1, Geography_Sec3 + 1):
 average_test_grade = total_test_grade / Geography_Sec3
 print("The average score for 3E Geography Test is " + str(average_test_grade) + ".")
 
-# Debugging
+Debugging
 
-# for i in range(3):
-#     print("Hello, World!")
+for i in range(3):
+    print("Hello, World!")
 
-# for i in range(5):
-#     print(i)
+for i in range(5):
+    print(i)
 
-# print("Hello, World!")
+print("Hello, World!")
 
-# x = 5
-# if x == 5:
-#     print("They are 5!")
+x = 5
+if x == 5:
+    print("They are 5!")
 
-# print ("Hello, World!")
+print ("Hello, World!")
 
-## Task 2: Name Errors
+# Task 2: Name Errors
 
-# age = 14.5
-# print(age)
-
-
-# name = "Alice"
-# print(name)
-
-# x = 5
-# print(x)
-
-# age = 25
-# print(age + 1)
+age = 14.5
+print(age)
 
 
-# number = 10
-# print(number - 5)
+name = "Alice"
+print(name)
 
-# Repeat = 3
-# repeat_num = 5
-# print(Repeat * str(repeat_num))
+x = 5
+print(x)
 
-
-# year = 2023
-# print("The year is " + str(year))
+age = 25
+print(age + 1)
 
 
-# x = 10
-# y = x / 2
+number = 10
+print(number - 5)
 
-# end = 5
-# for i in range(end):
-#     print(i)
+Repeat = 3
+repeat_num = 5
+print(Repeat * str(repeat_num))
+
+
+year = 2023
+print("The year is " + str(year))
+
+
+x = 10
+y = x / 2
+
+end = 5
+for i in range(end):
+    print(i)
