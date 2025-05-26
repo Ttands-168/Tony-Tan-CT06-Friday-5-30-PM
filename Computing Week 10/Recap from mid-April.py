207c1fbf04dfacd0d626887c130b81ff55ce436b
Ttands-168 <Tonydstan@gmail.com>
2025-05-26 00:30:43 +0000

    May 26, 2025, 12:30 AM

--- Computing Week 10/Recap from mid-April.py
+++ Computing Week 10/Recap from mid-April.py
@@ -2,4 +2,4 @@
 # Use underscores instead of hyphens in variable names
 box_size = 80 # not size_correct?
 print("Box size is", box_size)
-# 
+# You are discouraged from using break and continue in loops
