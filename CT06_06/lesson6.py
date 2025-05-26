bded9d343c657d90cd34a960432dfcd72b40ff3d
Ttands-168 <Tonydstan@gmail.com>
2025-05-26 00:39:02 +0000

    May 26, 2025, 12:39 AM

--- CT06_06/lesson6.py
+++ CT06_06/lesson6.py
@@ -1,16 +1,16 @@
 # For loop lesson 5
-# num1 = input("Give me your start value. ")
-# num2 = input("Give me your end value. ")
+num1 = input("Give me your start value. ")
+num2 = input("Give me your end value. ")
 
-# num1 = int(num1)
-# num2 = int(num2)
+num1 = int(num1)
+num2 = int(num2)
 
-# if num1 > num2:
-#     for Russia in range(num2, num1):
-#         print(Russia)
-# else:
-#     for Russia in range(num1, num2):
-#         print(Russia)
+if num1 > num2:
+    for Russia in range(num2, num1):
+        print(Russia)
+else:
+    for Russia in range(num1, num2):
+        print(Russia)
 
 # Geography Test
 
