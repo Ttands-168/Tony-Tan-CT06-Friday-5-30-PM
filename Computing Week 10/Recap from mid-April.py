98881cf143c9c2846becf08caa8ad1bd467af354
Ttands-168 <Tonydstan@gmail.com>
2025-05-26 00:31:04 +0000

    May 26, 2025, 12:31 AM

--- Computing Week 10/Recap from mid-April.py
+++ Computing Week 10/Recap from mid-April.py
@@ -1,5 +1,7 @@
+
 # Celeste and celeste are case-sensitive
 # Use underscores instead of hyphens in variable names
 box_size = 80 # not size_correct?
 print("Box size is", box_size)
 # You are discouraged from using break and continue in loops
+count = 10
