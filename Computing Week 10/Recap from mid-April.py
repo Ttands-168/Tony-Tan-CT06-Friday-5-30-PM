ab7c1e083fccfd7ddd8294839d942a7210cac09c
Ttands-168 <Tonydstan@gmail.com>
2025-05-29 13:46:18 +0000

    May 29, 2025, 1:46 PM

--- Computing Week 10/Recap from mid-April.py
+++ Computing Week 10/Recap from mid-April.py
@@ -55,6 +55,8 @@ if bone_age < 0 or current_height < 0:
     print("Bone age and height must be positive numbers.")  # Check for valid input
 if bone_age <= 1:
     print("You will grow by 20cm from now until you are 1 year old.")  # If bone age is less than or equal to 1, the user will grow by 20 cm
+elif bone_age <= 11:
+    print("You will grow by about 5-10 cm in the next year.") # 
 if bone_age <= 14:
     print("You are growing bigger and taller!")  # If bone age is less than or equal to 14, the user is growing
 elif bone_age <= 16:
