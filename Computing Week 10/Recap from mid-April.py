6f153bd980d4635346a486b340c33d53340d7f89
Ttands-168 <Tonydstan@gmail.com>
2025-05-29 13:21:53 +0000

    May 29, 2025, 1:21 PM

--- Computing Week 10/Recap from mid-April.py
+++ Computing Week 10/Recap from mid-April.py
@@ -54,4 +54,6 @@ current_height = float(input("What is your current height in metres? "))  # Ask 
 if bone_age < 0 or current_height < 0:
     print("Bone age and height must be positive numbers.")  # Check for valid input
 if bone_age <= 14:
-    print("You are growing ")
+    print("You are growing bigger and taller!")  # If bone age is less than or equal to 14, the user is growing
+elif bone_age <= 16:
+    print("You likely ")
